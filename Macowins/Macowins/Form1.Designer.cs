05bbe025471fa9f1e5c65673257e3ea35845299e
AgustinLabarinas <63302768+AgustinLabarinas@users.noreply.github.com>
2020-04-07 11:32:27 -0300

    Add files via upload

---
+++ Macowins/Macowins/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Macowins
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(148, 210);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 364);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(611, 210);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(513, 364);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vestimentas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(693, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Vendido";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 582);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Comprar en Efectivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seleccione lo que desea Comprar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(531, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Si compra algo en Liquidación al precio se le descontara un 50%.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(929, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 630);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 28);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Comprar con Tarjeta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(354, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Si compra con tarjeta se le hara un recargo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(538, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Si compra algo en Promoción al precio se le descontara un monto.";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(907, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 22);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Ganancias Del Dia";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(1258, 210);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(254, 364);
+            this.listBox3.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(763, 582);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 28);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Cerrar Dia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1306, 172);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 22);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Cierres de Dias";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1526, 684);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Macowins/Macowins/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Macowins
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Tienda T = new Tienda(); //Instancio la clase Tienda
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(Vestimenta v in T.ListVest)  //Imprimo en el Listbox cada prenda que hay en la lista
+            {
+                listBox1.Items.Add(v.Prenda +"  "+ v.Precio);
+            }
+
+            textBox1.Text = "0";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(T.CobrarEfec(listBox1.SelectedIndex)); // Llamo a la funcion para cobrar en efectivo pasandole el indice del listbox y el textbox
+            // donde guardo las ganancias del dia
+            textBox1.Text = T.Ganancia.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(T.CobrarTarj(listBox1.SelectedIndex)); // Llamo a la funcion para cobrar con tarjeta pasandole el indice del listbox y el textbox
+            //donde guardo las ganancias del dia
+            textBox1.Text = T.Ganancia.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Añado las ganancias del dia en en listbox y limpio las ventas del dia y el ingreso del dia.
+            listBox3.Items.Add(textBox1.Text + "  "+ DateAndTime.DateString);
+            listBox2.Items.Clear();
+            textBox1.Text = "0";
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Tienda //Clase tienda que contiene una lista de Vestimentas
+    {
+        public List<Vestimenta> ListVest;
+        public double Ganancia;
+
+        public Tienda()
+        {
+         ListVest = new List<Vestimenta>();  //Instancio la lista de vestimentas y añado un par a la lista.
+         Vestimenta A = new Vestimenta("Saco", 1000);
+         Vestimenta B = new Vestimenta("Camisa", 1000);
+         Vestimenta C = new Vestimenta("Pantalon", 1000);
+
+         ListVest.Add(A);
+         ListVest.Add(B);
+         ListVest.Add(C);
+
+         Ganancia = 0;
+        }
+
+        public string CobrarEfec (int index) //Funcion para cobrar en efectivo
+        {
+            int estado;
+            try
+            {
+                estado = int.Parse(Interaction.InputBox("Ingrese el numero 1 si es Nuevo, el 2 si esta en Promoción y 3 si esta en Liquidación: "));
+                double Vendido;
+
+
+                if (estado == 1)
+                {
+                    Vendido = ListVest[index].Precio;  //Me fijo el precio en la lista
+                    Ganancia = Ganancia + Vendido;  //Sumo en la variable ganancia las ventas del dia
+                    return (ListVest[index].Prenda + "  " + Vendido + "  Nuevo  " + DateAndTime.Now + "  Efectivo");
+                    
+                }
+                else if (estado == 2)
+                {
+                    Vendido = (ListVest[index].Precio - int.Parse(Interaction.InputBox("Ingrese el descuento por estar en promoción: ")));
+                    Ganancia = Ganancia + Vendido;
+                    return (ListVest[index].Prenda + "  " + Vendido + "  Promocion  " + DateAndTime.Now + "  Efectivo");
+                }
+
+                else if (estado == 3)
+                {
+                    Vendido = (ListVest[index].Precio / 2);
+                    Ganancia = Ganancia + Vendido;
+                    return (ListVest[index].Prenda + "  " + Vendido + "  Liquidación  " + "  " + DateAndTime.Now + "  Efectivo");
+                }
+                else
+                {
+                    MessageBox.Show("Ese estado no existe");
+                }
+            }
+            catch (Exception ex) { }
+
+            return "";
+        }
+
+        public string CobrarTarj (int index) // Funcion para pagar con tarjeta
+        {
+            try
+            {
+                int CantCuot;
+                CantCuot = int.Parse(Interaction.InputBox("Ingrese la cantidad de cuotas que desea realizar: "));
+                int estado;
+                estado = int.Parse(Interaction.InputBox("Ingrese el numero 1 si es Nuevo, el 2 si esta en Promoción y 3 si esta en Liquidación: "));
+                double Vendido;
+
+
+                if (estado == 1)
+                {
+                    Vendido = (ListVest[index].Precio + (CantCuot * 1.05 + ListVest[index].Precio / 100));  //Me fijo el precio en la lista y le agrego el recargo por usar tarjeta
+                    Ganancia = Ganancia + Vendido;
+                    return (ListVest[index].Prenda + "  " + Vendido + "  Nuevo  " + DateAndTime.Now + "  Tarjeta");
+                }
+                else if (estado == 2)
+                {
+                    double desc = int.Parse(Interaction.InputBox("Ingrese el descuento por estar en promoción: "));
+                    Vendido = ((ListVest[index].Precio - desc) + CantCuot * 1.05 + ((ListVest[index].Precio - desc) / 100));
+                    Ganancia = Ganancia + Vendido;
+                    return(ListVest[index].Prenda + "  " + Vendido + "  Promocion  " + DateAndTime.Now + "  Tarjeta");
+                }
+
+                else if (estado == 3)
+                {
+
+                    Vendido = ((ListVest[index].Precio / 2) + CantCuot * 1.05 + (ListVest[index].Precio / 2) / 100);
+                    Ganancia = Ganancia + Vendido;
+                    return (ListVest[index].Prenda + "  " + Vendido + "  Liquidación  " + "  " + DateAndTime.Now + "  Tarjeta");
+                }
+                else
+                {
+                    MessageBox.Show("Ese estado no existe");
+                }
+            }
+            catch (Exception ex) { }
+            return "";
+        }
+    }
+    public class Vestimenta // Clase vestimenta donde se especifica el tipo de la prenda y su precio
+    {
+        public string Prenda;
+        public double Precio;
+
+
+
+        public Vestimenta(string prend, double prec) //Constructor que obliga a mandar el tipo de prenda y el precio para poder instanciar una vestimenta
+        {
+            Prenda = prend;
+            Precio = prec;
+        }
+    }
+}
